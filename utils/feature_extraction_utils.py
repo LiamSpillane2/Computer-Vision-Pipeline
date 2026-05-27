8d00cb93468b56384a1266fb2d2f4cb1d0c6e265
Jack Hurt <datasciencebobby@gmail.com>
2026-05-27 07:54:24 -0400

    updated to add download model and image prediction functions

--- utils/feature_extraction_utils.py
+++ utils/feature_extraction_utils.py
@@ -2,10 +2,12 @@ import os
 import random
 from PIL import Image
 import numpy as np
+import pandas as pd
 import torch
-from transformers import AutoImageProcessor,  AutoModel
+from transformers import AutoImageProcessor,  AutoModel, pipeline
+import warnings
 
-def load_images(directory, num_img = 1, use_rand = True, seed = 42, gs = True, normalize = True):
+def load_images(directory, num_img = 1, use_rand = True, seed = 42, gs = True, normalize = True, img_obj = False):
     '''loads image file(s) from a directory, returns numpy array(s)
     
     Args:
@@ -53,20 +55,25 @@ def load_images(directory, num_img = 1, use_rand = True, seed = 42, gs = True, n
                 test_file = os.path.join(directory, valid_files[i])
             
             # open file
-            img = Image.open(test_file)
             
-            # define array based on gs parameter
-            if gs:
-                img_arr = np.asarray(img.convert("L"))
-            else:
-                img_arr = np.asarray(img.convert("RGB"))
+            img = Image.open(test_file)
 
-            # normalize array based on normalize parameter
-            if normalize:
-                img_arr = img_arr/255
+            if img_obj == True:
+                img_arr = img
+            
+            else:
+                # define array based on gs parameter
+                if gs:
+                    img_arr = np.asarray(img.convert("L"))
+                else:
+                    img_arr = np.asarray(img.convert("RGB"))
+
+                # normalize array based on normalize parameter
+                if normalize:
+                    img_arr = img_arr/255
             
             # add array to array list
-            img_arrays.append(img_arr)
+            img_arrays.append([img_arr, test_file])
 
         # return single array or array list based on num_img
         if num_img == 1:
@@ -80,7 +87,6 @@ def load_images(directory, num_img = 1, use_rand = True, seed = 42, gs = True, n
     except Exception as e:
         print(e)
 
-
 def get_image_embeddings(img_arr):
 
     os.environ["HF_HUB_VERBOSITY"] = "error"
@@ -88,6 +94,7 @@ def get_image_embeddings(img_arr):
     # 2. Suppress the Windows symlink warning
     os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
     os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
+    warnings.filterwarnings("ignore", category=UserWarning)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device {device}")
 
@@ -106,13 +113,76 @@ def get_image_embeddings(img_arr):
 
     return embedding
 
+def download_model(model = "openai/clip-vit-large-patch14"):
+    '''downloads model to device (defaul to openai CLIP)
+    
+    Args: model
+       
+    Returns: classifier
+    
+    Raises: 
+    '''
+    if torch.cuda.is_available():
+        model = r"openai/clip-vit-large-patch14"
+    else: 
+        model = r"openai/clip-vit-base-patch32"
+ 
+    # define model save path as downloads folder
+    local_directory = rf"C:/Users/{os.getlogin()}/Downloads/{model}"
+
+    # check if model is already saved
+    if not os.path.exists(local_directory):
+        
+        # save model to directory
+        classifier = pipeline(task="zero-shot-image-classification", model= model)
+        classifier.save_pretrained(local_directory)
+        print(f"Model successfully saved to {local_directory}")
+    
+    # get user input if model already saved locally
+    else:
+        response = input("Clip model already downloaded, would you like to redownload? (Y/N) ")
+
+        # redownload based on user input
+        if response.upper() == "Y":
+            classifier.save_pretrained(local_directory)
+            classifier = pipeline(task="zero-shot-image-classification", model= model)
+            print(f"Model successfully saved to: {local_directory}")
+        
+        # otherwise return
+        else:
+            classifier = pipeline(task="zero-shot-image-classification",model=local_directory)
+            print(f"Model already saved to: {local_directory}")
+        
+    return classifier
+
+def image_label_pred(image, label_list, classifier):
+    '''assigns probablility of an image matching a label based on a classifier
+    
+    Args: 
+    - image: image to be analyzed (PIL object)
+    - label_list: labels to be tested for probability
+    - classifier: CLIP image classifier
+       
+    Returns:
+    - dataframe with 
+    
+    Raises: 
+    '''
+
+    predictions = classifier(image, candidate_labels=label_list)
+    
+    # take list of dictionaries, convert to df and transpose
+    df = pd.DataFrame(predictions).T
+    
+    # column names are last row values
+    df.columns = df.iloc[-1]
+
+    # return all values except last row
+    return df[:-1]
+
 if __name__ == "__main__":
-    test_dir =  r"./data/formatted/license_plate_detection/train/images"
-    print("success!")
 
-    test_arr = load_images(test_dir, num_img= 3, gs = False, normalize= True)
+    print('main')
 
-    embeddings = get_image_embeddings(test_arr)
 
-    print(embeddings)
 
