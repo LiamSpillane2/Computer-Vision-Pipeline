a1282dfc74e8ad7a3a98f7099c4bbaf573a04ccd
cdartint123 <cdartint123@gmail.com>
2026-06-15 14:09:49 -0400

    Fixing lists

--- src/models/predict.py
+++ src/models/predict.py
@@ -2,9 +2,13 @@ from ultralytics import YOLO
 from pathlib import Path
 
 
-def run_model(path, save=False, conf=.25):
+def run_model(path, save=False, conf=.25,):
 
     model = YOLO(r"models\Yolo26Model\weights\best.pt")
+    
+    xyxy=[]
+    cls_id=[]
+    confidence=[]
 
     results = model.predict(source=path, save=save, conf=conf)
 
@@ -13,10 +17,12 @@ def run_model(path, save=False, conf=.25):
         boxes = result.boxes  # Bounding boxes object
         for box in boxes:
             # Get coordinates, class IDs, and confidence scores
-            xyxy = box.xyxy[0].tolist()
-            cls_id = int(box.cls[0])
-            confidence = float(box.conf[0])
+            xyxy.append(box.xyxy[0].tolist())
+            cls_id.append(int(box.cls[0]))
+            confidence.append(float(box.conf[0]))
     return xyxy, cls_id, confidence
 
 
-run_model(r"data\license_plate_detection\test\images\lp_test_001.jpg") # place image path into run model
+print(run_model(r"data\license_plate_detection\test\images\lp_test_001.jpg", save=True)) # place image path into run model
+
+
