a2d581f756f87fef01c855df4ee8af6cf6fd402a
Liam Spillane <liamspillane.ai@gmail.com>
2026-06-17 14:01:05 -0400

    move helper functions from src/single_image_pipeline.py to utils/pipeline_utils.py

--- src/single_image_pipeline.py
+++ src/single_image_pipeline.py
@@ -1,49 +1,13 @@
-import cv2
-import pandas as pd
 from src.models.predict import run_model
 from utils.ocr_utils import alpr_single_image
-from utils.zero_shot_utils import load_clip_pipeline, run_classification, prob_results
-
-
-def do_zero_shot(
-    image_folder: str,
-    results_path: str,
-    label_list: list[str],
-    label: str,
-    prob_thres: float = None,
-) -> tuple[pd.DataFrame, pd.DataFrame]:
-    classifier = load_clip_pipeline()
-    results = run_classification(
-        classifier=classifier,
-        image_folder=image_folder,
-        label_list=label_list,
-    )
-    p_results = prob_results(
-        results,
-        results_path,
-        label=label,
-        prob_thres=prob_thres,
-    )
-
-    return results, p_results
-
-
-def crop_image(image_path: str, bounding_boxes: list[str]) -> list:
-    image = cv2.imread(image_path)
-    cropped_images = []
-    for bounding_box in bounding_boxes:
-        x1, y1, x2, y2 = map(int, bounding_box)
-        cropped_image = image[y1:y2, x1:x2]
-        cropped_images.append(cropped_image)
-
-    return cropped_images
+from utils.pipeline_utils import do_zero_shot, crop_image
 
 
 def single_image_pipeline(
     image_path: str,
     model,
     zs_results_path: str,
-    zs_label_list: list,
+    zs_label_list: list[str],
     do_zs: bool = False,
     do_ocr: bool = False,
     save: bool = False,
